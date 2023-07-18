4752384d2316cdb57be455f0420c731e2bb2899d
Juan Carlos Hernandez <jchernandez0312@gmail.com>
2023-07-18 02:18:01 -0500

    Agregado navigationTitle a cada TabView

--- WhatsApp Clone SwiftUI/ContentView.swift
+++ WhatsApp Clone SwiftUI/ContentView.swift
@@ -8,26 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var tabTitle = Tab.status
     
     var body: some View {
-            TabView {
+        NavigationStack {
+            TabView(selection: $tabTitle) {
                 Text("Status")
                     .tabItem {
                         Image(systemName: "livephoto")
                         Text("Status")
                     }
+                    .tag(Tab.status)
                 
                 Text("Calls")
                     .tabItem {
                         Image(systemName: "phone.fill")
                         Text("Calls")
                     }
+                    .tag(Tab.calls)
                 
                 Text("Communities")
                     .tabItem {
                         Image(systemName: "person.3")
                         Text("Communities")
                     }
+                    .tag(Tab.communities)
                 
                 ChatView()
                     .badge(2)
@@ -35,19 +40,21 @@ struct ContentView: View {
                         Image(systemName: "message")
                         Text("Chats")
                     }
+                    .tag(Tab.chats)
                 
                 Text("Settings")
                     .tabItem {
                         Image(systemName: "gear")
                         Text("Settings")
                     }
+                    .tag(Tab.settings)
                 
-            }
+            }.navigationTitle(tabTitle.title)
             
         }
         
     }
-
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
@@ -55,5 +62,24 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-
-
+enum Tab: Int {
+    case status = 1
+    case calls = 2
+    case communities = 3
+    case chats = 4
+    case settings = 5
+    var title: String {
+        switch self {
+        case .status:
+            return "Status"
+        case .calls:
+            return "Calls"
+        case .communities:
+            return "Communities"
+        case .chats:
+            return "Chats"
+        case .settings:
+            return "Settings"
+        }
+    }
+}
