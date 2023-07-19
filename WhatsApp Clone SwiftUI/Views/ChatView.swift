3465bb794df01ddded2ee1ee22a12991c563ee80
Juan Carlos Hernandez <jchernandez0312@gmail.com>
2023-07-18 22:50:56 -0500

    Agregado search bar y botones inferiores

--- WhatsApp Clone SwiftUI/Views/ChatView.swift
+++ WhatsApp Clone SwiftUI/Views/ChatView.swift
@@ -8,13 +8,57 @@
 import SwiftUI
 
 struct ChatView: View {
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack {
+            SearchView()
+            HStack {
+                Button("Broadcast List", action: {})
+                Spacer()
+                Button("New Group", action: {})
+            }
+            .padding(.horizontal)
+            
+            Divider()
+            List {
+                
+            }
+        }
+        
+        
+        
     }
 }
 
 struct ChatView_Previews: PreviewProvider {
     static var previews: some View {
         ChatView()
+    }
+}
+
+struct SearchView: View {
+    
+    @State private var username: String = ""
+    
+    
+    var body: some View {
+        HStack {
+            HStack {
+                Image(systemName: "magnifyingglass")
+                    .foregroundColor(.gray)
+                TextField("Search", text: $username)
+                    .font(.system(size: 20))
+                
+            }
+            .padding(10)
+            .background(
+                RoundedRectangle(cornerRadius: 10)
+                    .fill(.gray.opacity(0.3))
+                
+            )
+            Image(systemName: "line.horizontal.3.decrease")
+                .foregroundColor(.blue)
+        }.padding()
     }
 }
