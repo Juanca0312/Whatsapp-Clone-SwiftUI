6a62bf1aeca8571f6e30d84f0093f40fd44b9377
Juan Carlos Hernandez <jchernandez0312@gmail.com>
2023-07-20 01:02:27 -0500

    Agregados chats tabs a vista

--- WhatsApp Clone SwiftUI/Views/ChatView.swift
+++ WhatsApp Clone SwiftUI/Views/ChatView.swift
@@ -22,8 +22,23 @@ struct ChatView: View {
             
             Divider()
             List {
+                Chat()
+                Chat()
+                Chat()
+                Chat()
+                Chat()
+                Chat()
+                Chat()
+                Chat()
+                Chat()
+                Chat()
+
+
+         
+
                 
-            }
+                
+            }.listStyle(.plain)
         }
         
         
@@ -60,5 +75,36 @@ struct SearchView: View {
             Image(systemName: "line.horizontal.3.decrease")
                 .foregroundColor(.blue)
         }.padding()
+    }
+}
+
+struct Chat: View {
+    var body: some View {
+        HStack {
+            Image("User")
+                .resizable()
+                .frame(width: 50, height: 50)
+                .clipShape(Circle())
+            
+            VStack(alignment: .leading) {
+                Text("Aldair")
+                HStack {
+                    Image(systemName: "checkmark")
+                    Text("Toma algo para")
+                }
+                
+            }
+            Spacer()
+            VStack (alignment: .trailing) {
+                Text("Yesterday")
+                Circle()
+                    .frame(width: 20, height: 20)
+                    .foregroundColor(.blue)
+                    .overlay(
+                        Text("2")
+                            .foregroundColor(.white)
+                    )
+            }
+        }
     }
 }
